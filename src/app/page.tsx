698d53a8ef796b8743a33bfde1cc5ac5ee3707c4
JORDAN <89118658+retcons@users.noreply.github.com>
2023-06-28 22:08:58 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
           due to identical entries, but not all. Additional comments from other
           sinners will be color-coded accordingly.
         </span>
-        <sup>last updated: canto iv part iii - june 22 2023</sup>
+        <sup>last updated: new mirror dungeon abnormality - june 28 2023</sup>
       </header>
       {/*
        * Because of React adding server components
