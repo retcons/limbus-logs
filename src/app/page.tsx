20542e8783093d99615851a4262d2088be7c3d0e
JORDAN <89118658+retcons@users.noreply.github.com>
2023-07-05 13:58:49 -0700

    small edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,7 @@ export default async function Home() {
           due to identical entries, but not all. Additional comments from other
           sinners will be color-coded accordingly.
         </span>
+        <sup>Managed by <a href="https://github.com/retcons" style="color:white;">jordan</a>, please direct any corrections, concerns, etc. to me.</sup>
         <sup>last updated: mirror dungeon 2 update - june 28 2023</sup>
       </header>
       {/*
