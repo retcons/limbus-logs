6dc587ba3140ec4219cbdb73f83f47c2e4d06ae1
April <95392008+apriltaoyvr@users.noreply.github.com>
2024-02-05 14:58:40 -0800

    Organizing components
    Move `Log` to `Abnormality/Log`
    Move `SinnerToggle` to its own component
    Add `value={selectedSinner}` to SinnerToggle

---
+++ src/app/components/Filter/SinnerToggle/index.tsx
@@ -0,0 +1,43 @@
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import type { Dispatch, SetStateAction } from 'react';
+import styles from './index.module.scss'
+
+type Props = { selectedSinner: string; setSelectedSinner: Dispatch<SetStateAction<string>> }
+
+export default function SinnerToggle({ selectedSinner, setSelectedSinner }: Props) {
+  return(
+    <ToggleGroup.Root
+      className={styles['toggle-group']}
+      type='single'
+      defaultValue='all'
+      value={selectedSinner}
+      aria-label='Log writer'
+      onValueChange={(value: string) => setSelectedSinner(value)}
+    >
+      <ToggleGroup.Item value={'all'} aria-label='All writers'>
+        All
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value={'yi_sang'} aria-label='Yi Sang'>
+        Yi Sang
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value={'faust'} aria-label='Faust'>
+        Faust
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value={'don_quixote'} aria-label='Don Quixote'>
+        Don Quixote
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value={'ryoshu'} aria-label='Ryōshū'>
+        Ryōshū
+      </ToggleGroup.Item>
+      <ToggleGroup.Item value={'meursault'}>Meursault</ToggleGroup.Item>
+      <ToggleGroup.Item value={'hong_lu'}>Hong Lu</ToggleGroup.Item>
+      <ToggleGroup.Item value={'heathcliff'}>Heathcliff</ToggleGroup.Item>
+      <ToggleGroup.Item value={'ishmael'}>Ishmael</ToggleGroup.Item>
+      <ToggleGroup.Item value={'rodion'}>Rodion</ToggleGroup.Item>
+      <ToggleGroup.Item value={'sinclair'}>Sinclair</ToggleGroup.Item>
+      <ToggleGroup.Item value={'outis'}>Outis</ToggleGroup.Item>
+      <ToggleGroup.Item value={'gregor'}>Gregor</ToggleGroup.Item>
+    </ToggleGroup.Root>
+
+  )
+}
