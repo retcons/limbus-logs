a6d84e07b7e3adeb7aae2300747f03256a101335
April <95392008+apriltaoyvr@users.noreply.github.com>
2024-02-04 23:36:15 -0800

    Update prisma

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,12 +1,6 @@
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
