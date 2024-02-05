ba73a963cd3c27adf9a861a7277d17a5e0920d0d
April <95392008+apriltaoyvr@users.noreply.github.com>
2024-02-04 23:14:16 -0800

    Updating for server-side hosting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* Static export
-   * This is so it can be hosted as html with js and css
-   * As opposed to requiring a server
-   */
-  output: 'export',
   images: { unoptimized: true },
 };
 
