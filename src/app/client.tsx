dc97d14905b48cd833390bfb8ff7b8b3878ade51
April <95392008+apriltaoyvr@users.noreply.github.com>
2025-05-19 15:51:28 -0700

    fix: Prisma schema, naming conventions, hydration, accessibility issues

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -10,46 +10,61 @@ export default function HomeClient({
 }: {
   abnormalities: AbnormalityProps[];
 }) {
-  // https://react.dev/reference/react/useState#examples-basic
-  // We are using React's useState to manage the state of the selected sinner
-  // aka Check what Sinner is currently selected in the filter
+  // Check what Sinner is currently selected in the filter
   const [search, setSearch] = useState('');
   const [selectedSinner, setSelectedSinner] = useState('all');
 
   // Filter down the abnormality list to match the search query AND if a sinner wrote a log for it
   const filteredAbnormalities = abnormalities.filter((abnormality) => {
-    const searchFilter = abnormality.name.toLowerCase().includes(search.toLowerCase());
-    const sinnerFilter = selectedSinner === 'all'
-      ? true
-      : abnormality.logs.some((log) => log.sinner_id === selectedSinner);
-    
+    const searchFilter = abnormality.name
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    const sinnerFilter =
+      selectedSinner === 'all'
+        ? true
+        : abnormality.logs.some((log) => log.sinnerId === selectedSinner);
+
     return searchFilter && sinnerFilter;
-  })
+  });
 
   return (
     <>
       <nav className={styles.filter}>
         <section className={styles['filter-section']}>
           <label className={styles['filter-label']}>Abnormality</label>
-          <input type='search' value={search} onChange={(e) => { setSearch(e.target.value) }} className={styles['search']} />
+          <input
+            type='search'
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            className={styles['search']}
+          />
         </section>
         <section className={styles['filter-section']}>
           <label className={styles['filter-label']}>Log Writer</label>
-          <SinnerToggle selectedSinner={selectedSinner} setSelectedSinner={setSelectedSinner} />
+          <SinnerToggle
+            selectedSinner={selectedSinner}
+            setSelectedSinner={setSelectedSinner}
+          />
         </section>
       </nav>
       <main className={styles.gallery}>
-        {filteredAbnormalities.length > 0 ? (filteredAbnormalities
-          /* Return the abnormalities that meet this criteria with the Abnormality component */
-          .map((abnormality) => {
+        {filteredAbnormalities.length > 0 ? (
+          // Return the abnormalities that meet this criteria with the Abnormality component
+          filteredAbnormalities.map((abnormality) => {
             return (
               <Abnormality abnormality={abnormality} key={abnormality.name} />
             );
-          }))
-          : (
-            // If no results are found, display a message
-            <span className={styles.noresult}>No results found for <strong >{search === '' ? 'anything' : search}</strong> by <strong>{idToName(selectedSinner)}</strong> </span>
-          )}
+          })
+        ) : (
+          // If no results are found, display a message
+          <span className={styles.noresult}>
+            No results found for{' '}
+            <strong>{search === '' ? 'anything' : search}</strong> by{' '}
+            <strong>{idToName(selectedSinner)}</strong>{' '}
+          </span>
+        )}
       </main>
     </>
   );
